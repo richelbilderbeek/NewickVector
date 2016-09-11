cf6c0e06867b45411440efd3c013e4ea1b28081d
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-11 18:45:05 +0200

    Do install other GitHubs

---
+++ install_other_githubs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ..
+
+git clone https://github.com/richelbilderbeek/RibiClasses
+git clone https://github.com/richelbilderbeek/RibiLibraries
+git clone https://github.com/richelbilderbeek/BinaryNewickVector
+git clone https://github.com/richelbilderbeek/Newick
+git clone https://github.com/richelbilderbeek/TwoDigitNewick
